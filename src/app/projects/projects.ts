4659f42936ac0f5b6c53705b4dc11252225b8050
Ghislain Z. <gzeleu@outlook.de>
2025-11-19 14:39:53 +0100

    FORMAT

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {
-
-}
+export class Projects {}
--- src/app/resume/resume.ts
+++ src/app/resume/resume.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './resume.html',
   styleUrl: './resume.scss',
 })
-export class Resume {
-
-}
+export class Resume {}
